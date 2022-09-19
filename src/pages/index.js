8a019c4ca2ae7a87d609765a96f490604c25a749
wnhollington <wnhollington@gmail.com>
2022-09-18 19:14:00 -0600

    make index page content full width

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({ data }) => {
     <Layout>
 
       {/* Hero */}
-      <section className="px-4 py-4 lg:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <section className="py-4 lg:py-16 mx-auto lg:py-20 w-full">
         <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
           <div className="flex flex-col justify-center">
             <div className="max-w-xl mb-6">
@@ -59,7 +59,7 @@ const IndexPage = ({ data }) => {
       </section>
 
       {/* Recent Posts */}
-      <section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <section className="py-4 lg:py-16 mx-auto lg:py-20 w-full">
         <div className="max-w-xl mb-10 md:mx-auto text-center lg:max-w-2xl md:mb-12">
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
             Featured Posts
