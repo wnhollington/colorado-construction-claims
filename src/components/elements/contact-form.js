352e6a69a3f6f66a145e562bade52ff2b076bd64
wnhollington <wnhollington@gmail.com>
2022-09-20 07:02:44 -0600

    remove captcha

--- src/components/elements/contact-form.js
+++ src/components/elements/contact-form.js
@@ -27,7 +27,6 @@ const ContactForm = () => (
                 <textarea type="text" name="message" id="message" className="h-36 text-base leading-none text-gray-900 p-3 focus:outline-none focus:border-primary-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none" required></textarea>
             </div>
         </div>
-        <div data-netlify-captcha></div>
         <p className="text-xs leading-3 text-gray-600 dark:text-gray-200 mt-4">By clicking submit you agree to our terms of service, privacy policy and how we use data as stated</p>
         <div className="flex items-center justify-center w-full">
             <button type="submit" name="submit" id="submit" className="mt-9 text-base font-semibold leading-none text-white py-4 px-10 bg-primary-700 rounded hover:bg-primary-600 focus:ring-2 focus:ring-offset-2 focus:ring-primary-700 focus:outline-none w-full">SUBMIT</button>
