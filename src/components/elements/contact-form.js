ee7cc876841ccab14824e414b36fb9d5640a66fa
wnhollington <wnhollington@gmail.com>
2022-09-20 06:52:40 -0600

    add captcha to contact form

--- src/components/elements/contact-form.js
+++ src/components/elements/contact-form.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 const ContactForm = () => (
 
-    <form name="contact" method="POST" className="bg-white dark:bg-gray-800 py-6 lg:px-28 px-8" data-netlify="true">
+    <form name="contact" method="POST" className="bg-white dark:bg-gray-800 py-6 lg:px-28 px-8" data-netlify="true" netlify-honeypot="bot-field">
         <input type="hidden" name="contact" value="contact"/>
         <div className="md:flex items-center mt-12">
             <div className="md:w-72 flex flex-col">
@@ -26,6 +26,7 @@ const ContactForm = () => (
                 <textarea type="text" name="message" id="message" className="h-36 text-base leading-none text-gray-900 p-3 focus:outline-none focus:border-primary-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none" required></textarea>
             </div>
         </div>
+        <div data-netlify-captcha></div>
         <p className="text-xs leading-3 text-gray-600 dark:text-gray-200 mt-4">By clicking submit you agree to our terms of service, privacy policy and how we use data as stated</p>
         <div className="flex items-center justify-center w-full">
             <button type="submit" name="submit" id="submit" className="mt-9 text-base font-semibold leading-none text-white py-4 px-10 bg-primary-700 rounded hover:bg-primary-600 focus:ring-2 focus:ring-offset-2 focus:ring-primary-700 focus:outline-none w-full">SUBMIT</button>
