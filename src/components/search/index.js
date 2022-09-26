9b0984012aa6b8d614f5721d10f9cd26589efa81
wnhollington <wnhollington@gmail.com>
2022-09-25 18:28:00 -0600

    structure blog to grid layout

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,6 +1,6 @@
 import algoliasearch from "algoliasearch/lite"
 import { default as React, useState, useMemo } from "react"
-import { InstantSearch, SearchBox, Menu, Pagination } from "react-instantsearch-dom"
+import { InstantSearch, SearchBox, connectMenu, Pagination } from "react-instantsearch-dom"
 import SearchResult from "./search-results"
 
 export default function Search({ indices }) {
@@ -15,23 +15,70 @@ export default function Search({ indices }) {
     []
   )
 
+  const Menu = ({ items, currentRefinement, refine, attribute}) => (
+    <div className="bg-gray-100 p-4 my-2 rounded-lg">
+      <h2 className="underline underline-offset-8 text-gray-900 font-bold">{attribute}</h2>
+      <ul className="my-2 p-2">
+        {items.map(item => (
+          <li 
+            key={item.value}
+            className="my-2 py-2 border-b"
+          >
+            <a
+              href="#"
+              onClick={event => {
+                event.preventDefault();
+                refine(item.value);
+              }}
+              className={item.label === currentRefinement ? "text-primary-800" : "text-gray-900 hover:text-primary-800"}
+            >
+              {item.label} ({item.count})
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  const CustomMenu = connectMenu(Menu)
+
   return (
     <InstantSearch
       searchClient={searchClient}
       indexName={indices[0].name}
       onSearchStateChange={({ query }) => setQuery(query)}
     >
-      <div className="grid gap-5 row-gap-5 grid-cols-4">
+      <div className="grid gap-5 row-gap-5 grid-cols-1 lg:grid-cols-4">
         <section className="col-span-3">
           <SearchResult
             show={query && query.length > 0 && hasFocus}
             indices={indices}
           />
-          <Pagination/>
+	
+
+          <Pagination
+            translations={{
+              previous: '‹',
+              next: '›',
+              first: '«',
+              last: '»',
+              page(currentRefinement) {
+                return currentRefinement;
+              },
+              ariaPrevious: 'Previous page',
+              ariaNext: 'Next page',
+              ariaFirst: 'First page',
+              ariaLast: 'Last page',
+              ariaPage(currentRefinement) {
+                return `Page ${currentRefinement}`;
+              },
+            }}
+          />
+
         </section>
-        <section class="flex flex-col my-4">
+        <section class="flex flex-col my-4 order-first lg:order-last">
           <SearchBox />
-          <Menu attribute="category"/>
+          <CustomMenu attribute="category"/>
+          <CustomMenu attribute="tags"/>
         </section>
       </div>
 
